405a4e6a38281282648a8e88c8270b1b950478f4
Antonio Zapata <antoniojosezapata@gmail.com>
2019-06-06 18:12:19 +0200

    deberes-js-clase1

---
+++ Deberes/deberes-javascript/js/script.js
@@ -0,0 +1,17 @@
+/* Necesito preguntar al usuario el nombre  la edad de cada estudiante, 
+luego lanzar un mensaje con los datos resultantes de cada uno */
+
+var nombredevictoria = prompt("¿Cual es tu nombre");
+var edaddevictoria = prompt("¿Cual es tu edad?");
+var nombredeantonio = prompt("¿Cual es tu nombre");
+var edaddeantonio = prompt("¿Cual es tu edad?");
+var nombredetexy = prompt("¿Cual es tu nombre");
+var edaddetexy = prompt("¿Cual es tu edad?");
+var nombredeharving = prompt("¿Cual es tu nombre");
+var edaddeharving = prompt("¿Cual es tu edad?");
+var nombredeibrahima = prompt("¿Cual es tu nombre");
+var edaddeibrahima = prompt("¿Cual es tu edad?");
+alert(nombredevictoria + " tiene " + edaddevictoria + " años, " + nombredeantonio + " tiene " + edaddeantonio + " años, " + nombredetexy + " tiene " + edaddetexy + " años, " + nombredeharving + " tiene " + edaddeharving + " años, " + nombredeibrahima + " tiene " + edaddeibrahima + " años, ")
+
+
+
