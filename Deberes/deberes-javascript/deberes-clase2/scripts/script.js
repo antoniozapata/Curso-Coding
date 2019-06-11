e660d3de4e96cba3fe301dbd02001ae929775690
Antonio Zapata <48960021+antoniozapata@users.noreply.github.com>
2019-06-11 17:20:42 +0200

    deber 1 de la clase 2 js

---
+++ Deberes/deberes-javascript/deberes-clase2/scripts/script.js
@@ -0,0 +1,18 @@
+/* Ejercicio 1 - La calculadora de suministro de por vida
+¿Cuántas papas comerá una persona hasta el final de su vida? ¡Descubrete!
+
+Almacena la edad actual de las personas en una variable.
+Almacena una edad máxima estimada en una variable.
+Almacene una cantidad estimada por semana (como un número).
+Calcule cuántas semanas pasarán hasta el final de la vida de esta persona.
+Calcule cuántas papas se comerían en total durante el resto de su vida.
+Envíe el resultado a la pantalla mediante una consola como la siguiente: "NAME tiene NN años y se comerán NN papas hasta la edad de X". */
+
+let name = "Antonio";
+let age = 34;
+const MAXAGE = 90;
+let numPerWeek = 7;
+const WEEKSFORLIFE = (MAXAGE - age) * (52)
+let totalNeeded = numPerWeek * WEEKSFORLIFE;
+let message = name + " tiene " + age + " años y se comera " + totalNeeded + " papas hasta la edad de " + MAXAGE
+console.log(message);
