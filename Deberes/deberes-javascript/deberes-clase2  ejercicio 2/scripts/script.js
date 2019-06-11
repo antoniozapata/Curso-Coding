ddccf3ca60595efb224feb469b2390b45a3fd8a0
Antonio Zapata <antoniojosezapata@gmail.com>
2019-06-11 19:53:51 +0200

    deberes clase 2 ejercicio 2 y bonus

--- Deberes/deberes-javascript/deberes-clase2  ejercicio 2/scripts/script.js
+++ Deberes/deberes-javascript/deberes-clase2  ejercicio 2/scripts/script.js
@@ -6,33 +6,43 @@ Pregunte: "Los peces solo tienen tres segundos de memoria: verdadero o falso". S
 Pregunte: "La Gran Muralla China es la única estructura hecha por el hombre visible desde el espacio La Gran Muralla China: verdadero o falso". Si esa respuesta es "verdadera", se muestra "correcta", de lo contrario se muestra "incorrecta".
 Crea más preguntas verdaderas o falsas. */
 
+let points = 0;
+
 const ISTOMATOS = prompt("Tomatos are fruits, not vegetables: true or false?");
-if (ISTOMATOS === true) {
+if (ISTOMATOS === "true") {
   console.log('correcto');
+  points = points + 10;
 } else {
   console.log('incorrecto');
+  points = points - 5;
 }
 
 const DRINK8GLASSES = prompt("You should drink 8 glasses of water: true or false?");
 
-if (DRINK8GLASSES === true) {
+if (DRINK8GLASSES === "true") {
   console.log('correcto');
+  points = points + 10;
 } else {
   console.log('incorrecto');
+  points = points - 5;
 }
 
 const SECONDSOFMEMORY = prompt("Fishes have only three seconds of memory: true or false?");
 
-if (SECONDSOFMEMORY === true) {
+if (SECONDSOFMEMORY === "true") {
   console.log('correcto');
+  points = points + 10;
 } else {
   console.log('incorrecto');
+  points = points - 5;
 }
 
 const GREATWALL = prompt("The Great Wall of China is the only man made structure visible from space The Great Wall of China: true or false?");
 
-if (GREATWALL === true) {
+if (GREATWALL === "true") {
   console.log('correcto');
+  points = points + 10;
 } else {
   console.log('incorrecto');
+  points = points - 5;
 }
