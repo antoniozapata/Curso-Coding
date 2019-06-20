84d6bf5ec13f10f43691daccf4316885b8561701
Antonio Zapata <antoniojosezapata@gmail.com>
2019-06-20 21:17:58 +0200

    reduccion de codigo

--- Deberes/deberes-javascript/deberes-clase5 rainbow/scripts/script.js
+++ Deberes/deberes-javascript/deberes-clase5 rainbow/scripts/script.js
@@ -1,43 +1,38 @@
 const divRed = document.createElement("div");
 const contenedor = document.getElementsByClassName("my-container")[0];
-const alto = contenedor.getElementsByTagName("div");
 contenedor.appendChild(divRed);
 
-divRed.style.backgroundColor = ("red");
-divRed.style.width = ("400px");
-divRed.style.height = ("40px");
+divRed.style.backgroundColor = "red";
+
 
 const divOrange = document.createElement("div");
 contenedor.appendChild(divOrange);
 
 divOrange.style.backgroundColor = ("orange");
-divOrange.style.width = ("400px");
-divOrange.style.height = ("40px");
 
 const divYellow = document.createElement("div");
 contenedor.appendChild(divYellow);
 
 divYellow.style.backgroundColor = ("yellow");
-divYellow.style.width = ("400px");
-divYellow.style.height = ("40px");
 
 const divGreen = document.createElement("div");
 contenedor.appendChild(divGreen);
 
 divGreen.style.backgroundColor = ("green");
-divGreen.style.width = ("400px");
-divGreen.style.height = ("40px");
 
 const divBlue = document.createElement("div");
 contenedor.appendChild(divBlue);
 
 divBlue.style.backgroundColor = ("blue");
-divBlue.style.width = ("400px");
-divBlue.style.height = ("40px");
 
 const divPurple = document.createElement("div");
 contenedor.appendChild(divPurple);
 
 divPurple.style.backgroundColor = ("purple");
-divPurple.style.width = ("400px");
-divPurple.style.height = ("40px");
+
+const divs = [divRed, divOrange, divYellow, divGreen, divBlue, divPurple];
+
+divs.forEach((div) => {
+    div.style.width = "400px";
+    div.style.height = "40px";
+});
