6993f1868e71b629fcb3b900a502afa2067a16c1
Antonio Zapata <48960021+antoniozapata@users.noreply.github.com>
2019-07-09 16:50:59 +0200

    Arreglo

---
+++ Deberes/deberes-javascript/deberes-clase9 varios ejercicios/scripts/script.js
@@ -0,0 +1,19 @@
+function sum() {
+    const n1 = 5;
+    const n2 = 6;
+    result = n1 + n2;
+}
+
+function substract() {
+    const n1 = 5;
+    const n2 = 6;
+    result = n1 - n2;
+}
+
+function substract() {
+    const n1 = 20;
+    const n2 = 5;
+    result = n1 / n2;
+}
+
+
